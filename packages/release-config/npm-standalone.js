7ad238e3fd03b294e90e099c846a84ad5ed756bd
murshidazher <hello@murshidazher.com>
2023-05-27 23:46:04 +0530

    feat: add public npm release standalone config

---
+++ packages/release-config/npm-standalone.js
@@ -0,0 +1,25 @@
+module.exports = {
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [{ type: 'test', release: 'patch' }]
+      }
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@amanda-mitchell/semantic-release-npm-multiple',
+      {
+        registries: {
+          public: {}
+        }
+      }
+    ],
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      { message: `chore(release): \${nextRelease.gitTag} [skip ci]` }
+    ]
+  ]
+}
